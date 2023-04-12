3b1b1b937d2784c7ee2c08816edb0ac9c702dda9
vpetrovaa <vpetrova.laba@solvd.com>
2023-04-13 00:45:12 +0300

    Method with calling external method was fixed

--- src/main/java/com/solvd/carsharing/command/impl/CommandServiceImpl.java
+++ src/main/java/com/solvd/carsharing/command/impl/CommandServiceImpl.java
@@ -6,16 +6,13 @@ import com.solvd.carsharing.command.CreateCarCommand;
 import com.solvd.carsharing.command.CommandService;
 import com.solvd.carsharing.command.RentCarCommand;
 import com.solvd.carsharing.command.UpdateNumberCommand;
+import com.solvd.carsharing.command.util.CommandUtils;
 import com.solvd.carsharing.domain.Car;
 import com.solvd.carsharing.event.Event;
 import com.solvd.carsharing.event.EventService;
-import com.solvd.carsharing.kafka.property.KfProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -27,7 +24,7 @@ public class CommandServiceImpl implements CommandService {
 
     private final AggregateService aggregateService;
     private final EventService eventService;
-    private final KfProperties properties;
+    private final CommandUtils commandUtils;
 
     @Override
     public Mono<CarAggregate> handleCreateCarCommand(Car car) {
@@ -54,28 +51,18 @@ public class CommandServiceImpl implements CommandService {
         aggregateService.findByNumber(number)
                 .map(car -> {
                     if(car.getStatus().equals(Car.Status.FREE)) {
-                        callConfirmOrDenyMethod(aggregateId, "confirm");
+                        commandUtils.callConfirmOrDenyMethod(aggregateId, "confirm");
                         RentCarCommand command = new RentCarCommand();
                         command.setAggregateId(car.getId());
                         command.setCarNumber(number);
                         Event event = eventService.rent(command);
                         return aggregateService.rent(event, number);
                     } else {
-                        callConfirmOrDenyMethod(aggregateId, "deny");
+                        commandUtils.callConfirmOrDenyMethod(aggregateId, "deny");
                         return aggregate;
                     }
                 });
         return aggregate;
-    }
-
-    public void callConfirmOrDenyMethod(String aggregateId, String value){
-        WebClient webClient = WebClient.create("http://" + properties.getHost());
-        Mono<String> confirm = webClient
-                .get()
-                .uri("/api/v1/rentals/" + value + "/" + aggregateId)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .retrieve()
-                .bodyToMono(String.class);
     }
 
 }
---
+++ src/main/java/com/solvd/carsharing/command/util/CommandUtils.java
@@ -0,0 +1,7 @@
+package com.solvd.carsharing.command.util;
+
+public interface CommandUtils {
+
+    public void callConfirmOrDenyMethod(String aggregateId, String value);
+
+}
